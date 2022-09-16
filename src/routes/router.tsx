25fefb1a3b4182e20b2da63ebdf959e96d13207a
jmamadeu <jmamadeu2000@gmail.com>
2022-09-16 04:44:53 +0100

    chore: add home screen navigation

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { HomeScreen } from '~/screens/home/home';
 import { OnboardingScreen } from '~/screens/onboarding/onboarding';
 import { SignInScreen } from '~/screens/sign-in/sign-in';
 
 export type RootStackParamList = {
   Onboarding: undefined;
   SignIn: undefined;
+  Home: undefined;
 };
 
 const MainStack = createNativeStackNavigator<RootStackParamList>();
@@ -15,6 +17,7 @@ export const Router = () => (
     <MainStack.Group screenOptions={{ headerShown: false }}>
       <MainStack.Screen component={OnboardingScreen} name="Onboarding" />
       <MainStack.Screen component={SignInScreen} name="SignIn" />
+      <MainStack.Screen component={HomeScreen} name="Home" />
     </MainStack.Group>
   </MainStack.Navigator>
 );
--- src/screens/sign-in/sign-in.tsx
+++ src/screens/sign-in/sign-in.tsx
@@ -1,11 +1,21 @@
 import { FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Button, ButtonText } from '~/button/button';
+import type { RootStackParamList } from '~/routes/router';
 import { theme } from '~/theme';
 
+type SignInScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'SignIn'>;
+
 export const SignInScreen = () => {
+  const navigate = useNavigation<SignInScreenNavigationProp>();
+
+  const handleSocialLoginButtonClick = () => {
+    navigate.navigate('Home');
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View>
@@ -15,7 +25,11 @@ export const SignInScreen = () => {
         </View>
 
         <View style={styles.buttonsContainer}>
-          <Button style={[styles.getStartedButton]} activeOpacity={0.8}>
+          <Button
+            onPress={handleSocialLoginButtonClick}
+            style={[styles.getStartedButton]}
+            activeOpacity={0.8}
+          >
             <FontAwesome name="facebook" size={18} color={theme.colors.white} />
 
             <ButtonText style={styles.buttonText}>Facebook</ButtonText>
@@ -23,14 +37,22 @@ export const SignInScreen = () => {
 
           <View style={styles.divider} />
 
-          <Button style={styles.googleButton} activeOpacity={0.8}>
+          <Button
+            onPress={handleSocialLoginButtonClick}
+            style={styles.googleButton}
+            activeOpacity={0.8}
+          >
             <FontAwesome name="google" size={18} color={theme.colors.white} />
             <ButtonText style={styles.buttonText}>Google</ButtonText>
           </Button>
 
           <View style={styles.divider} />
 
-          <Button style={styles.appleButton} activeOpacity={0.8}>
+          <Button
+            onPress={handleSocialLoginButtonClick}
+            style={styles.appleButton}
+            activeOpacity={0.8}
+          >
             <FontAwesome name="apple" size={18} color={theme.colors.white} />
             <ButtonText style={styles.buttonText}>Apple</ButtonText>
           </Button>
