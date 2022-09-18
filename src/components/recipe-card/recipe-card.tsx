b1f0a6c09a817418157cda74c28e891402477462
jmamadeu <jmamadeu2000@gmail.com>
2022-09-19 00:01:40 +0100

    chore: add home page

--- src/components/recipe-card/recipe-card.tsx
+++ src/components/recipe-card/recipe-card.tsx
@@ -1,9 +1,71 @@
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import { theme } from '~/theme';
 
 export const RecipeCard = () => {
   return (
     <View>
-      <Text>Recipe 1</Text>
+      <View style={styles.authorContainer}>
+        <Image
+          source={{ uri: 'https://avatars.githubusercontent.com/u/59426856?v=4' }}
+          style={styles.image}
+        />
+        <Text style={styles.authorName}>João Amadeu</Text>
+      </View>
+
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.recipeImage}
+          source={{
+            uri: 'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+          }}
+        />
+        <TouchableOpacity style={styles.favoriteButton} activeOpacity={0.8}>
+          <Feather name="heart" size={20} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text style={[theme.typography.h2, styles.recipeName]}>Angolan Salad</Text>
+        <Text style={theme.typography.p2}>Food &gt; 60 min</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  image: { width: 32, height: 32, borderRadius: 10 },
+  authorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16
+  },
+  authorName: {
+    ...theme.typography.s,
+    color: theme.colors.mainText,
+    marginLeft: 8
+  },
+  imageContainer: {
+    marginBottom: 16,
+    position: 'relative'
+  },
+  recipeImage: { width: 150, height: 150, borderRadius: 16 },
+  recipeName: {
+    marginBottom: 8
+  },
+  favoriteButton: {
+    position: 'absolute',
+    left: 110,
+    top: 10,
+
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    borderRadius: 8,
+
+    height: 32,
+    width: 32,
+
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,10 +1,13 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Feather } from '@expo/vector-icons';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Platform, NativeModules, StatusBar } from 'react-native';
+import { FlashList } from '@shopify/flash-list';
 
 import { TextInput } from '~/components/text-input/text-input';
 import { theme } from '~/theme';
 import { Chip } from '~/components/button/chips';
+import { RecipeCard } from '~/components/recipe-card/recipe-card';
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 50 : StatusBar.currentHeight;
 
 const data = [
   {
@@ -35,37 +38,46 @@ const data = [
 
 export const HomeScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.headerContainer}>
+    <View style={styles.container}>
+      <View style={styles.paddingHorizontal}>
+        <TextInput
+          viewStyle={styles.inputSearch}
+          placeholder="Search"
+          left={<Feather name="search" size={22} color={theme.colors.blue[100]} />}
+        />
+      </View>
+
+      <View style={styles.categoryContainer}>
         <View style={styles.paddingHorizontal}>
-          <TextInput
-            viewStyle={styles.inputSearch}
-            placeholder="Search"
-            left={<Feather name="search" size={22} color={theme.colors.blue[100]} />}
+          <Text style={theme.typography.h2}>Category</Text>
+        </View>
+
+        <View style={styles.categoryButtons}>
+          <FlatList
+            data={data}
+            contentContainerStyle={{ paddingHorizontal: 24 }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Chip selected={item.selected} style={styles.categoryButton}>
+                {item.label}
+              </Chip>
+            )}
           />
         </View>
-
-        <View style={styles.categoryContainer}>
-          <View style={styles.paddingHorizontal}>
-            <Text style={theme.typography.h2}>Category</Text>
-          </View>
-
-          <View style={styles.categoryButtons}>
-            <FlatList
-              data={data}
-              contentContainerStyle={{ paddingHorizontal: 24 }}
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              renderItem={({ item }) => (
-                <Chip selected={item.selected} style={styles.categoryButton}>
-                  {item.label}
-                </Chip>
-              )}
-            />
-          </View>
-        </View>
       </View>
-    </SafeAreaView>
+
+      <View style={[styles.paddingHorizontal, styles.recipesContainer]}>
+        <FlashList
+          renderItem={() => <RecipeCard />}
+          estimatedItemSize={50}
+          data={[1, 2, 3, 4, 5, 6]}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={{ height: 32 }} />}
+        />
+      </View>
+    </View>
   );
 };
 
@@ -73,12 +85,12 @@ export const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.white,
     flex: 1,
-    paddingTop: 16
+    paddingTop: STATUSBAR_HEIGHT
   },
   paddingHorizontal: {
     marginHorizontal: 24
   },
-  headerContainer: {},
+
   inputSearch: {
     backgroundColor: theme.colors.grey[100]
   },
@@ -87,6 +99,10 @@ export const styles = StyleSheet.create({
   },
   categoryButtons: { flexDirection: 'row', marginTop: 16 },
   categoryButton: {
-    marginRight: 16
+    marginRight: 8
+  },
+  recipesContainer: {
+    marginTop: 30,
+    flex: 1
   }
 });
